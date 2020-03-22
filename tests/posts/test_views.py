b859b11562a772ed93223fe2bc57eae3c3d3e04c
captain204 <nurudeenakindele8@gmail.com>
2020-03-22 20:03:08 +0100

    templates added

--- tests/posts/test_views.py
+++ tests/posts/test_views.py
@@ -1,7 +1,10 @@
 from flask import url_for
+from lib.tests import assert_status_with_message, ViewTestMixin
+from application.blueprints.users.models import User
 
 
-class TestPosts(object):
+
+class TestPosts(ViewTestMixin):
     def test_post_page(self,client):
         """Post page should respond with a 200"""
         response = client.get(url_for('mypost.post'))
@@ -14,7 +17,7 @@ class TestPosts(object):
 
     def test_post_delete_page(self,client):
         """Post delete route should respond with a 200"""
-        response = client.get(url_for('mypost.delete', id = 1))
+        response = client.get(url_for('mypost.delete'))
         assert response.status_code == 200
     
     def test_post_form(self,client):
@@ -32,6 +35,6 @@ class TestPosts(object):
         form = {
             'body':'Testing post form'
         }
-        response = client.post(url_for('mypost.update',id =1), data=form,
+        response = client.post(url_for('mypost.update'), data=form,
                                 follow_redirects=True)
         assert response.status_code == 200
